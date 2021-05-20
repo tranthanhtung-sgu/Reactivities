38ef9ef23ad5305571e93a74cfd5ca2cfa50162a
Trần Thanh Tùng <50178152+tranthanhtung-sgu@users.noreply.github.com>
2021-05-20 19:57:37 +0700

    Create CURD In React

---
+++ client-app/src/feature/activities/form/ActivityForm.tsx
@@ -0,0 +1,43 @@
+import React, { ChangeEvent, useState } from 'react';
+import { Button, Form, Segment } from 'semantic-ui-react';
+import { Activity } from '../../../app/layout/models/activity';
+
+interface Props {
+    closeForm: () => void;
+    activity: Activity | undefined;
+    createOrEdit: (activity: Activity) => void;
+}
+
+export default function ActivityForm({closeForm, activity: setSelectedActivity, createOrEdit}:Props) {
+    const initialState = setSelectedActivity ?? {
+        id: '',
+        title: '',
+        category: '',
+        city: '',
+        date: '',
+        description: '',
+        venue: ''
+    }
+    const [activity, setActivity] = useState(initialState);
+    function handleSubmit() {
+        createOrEdit(activity);
+    }
+    function handleInputChange (event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) {
+        const {name, value} = event.target;
+        setActivity({...activity, [name]: value})
+    }
+    return (
+        <Segment clearing>
+            <Form onSubmit={handleSubmit} >
+                <Form.Input placeholder='Title' value={activity.title} name="title" onChange={handleInputChange} />
+                <Form.TextArea placeholder='Description' value={activity.description} name="description" onChange={handleInputChange} />
+                <Form.Input placeholder='Category' value={activity.category} name="category" onChange={handleInputChange} />
+                <Form.Input placeholder='Date' value={activity.date} name="date" onChange={handleInputChange} />
+                <Form.Input placeholder='City' value={activity.city} name="city" onChange={handleInputChange} />
+                <Form.Input placeholder='Venue' value={activity.venue} name="venue" onChange={handleInputChange} />
+                <Button onClick={closeForm} floated='right' type='button' content='Cancel' />
+                <Button floated='right' positive type='submit' content='Submit' />
+            </Form>
+        </Segment>
+    )
+}
